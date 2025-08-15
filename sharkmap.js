b3e864c6d9b0351e11ec37ba2f1ee887018ee61a
MinimalMule <51201295+MinimalMule@users.noreply.github.com>
2025-08-16 07:59:11 +0800

    Create
    nothing

---
+++ sharkmap.js
@@ -0,0 +1,6 @@
+import * as globeletjs from "https://unpkg.com/globeletjs@latest/dist/globelet.js";
+const params = { container: 'globe',
+style: "./klokantech-basic-style-geojson.json",
+center: [-100, 38.5],
+altitude: 6280,
+};
