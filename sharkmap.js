751ed43f038cb26d6c2fbfb82fc7fce8721fcc13
MinimalMule <51201295+MinimalMule@users.noreply.github.com>
2025-08-16 08:17:21 +0800

    merge

--- sharkmap.js
+++
@@ -1,7 +0,0 @@
-import * as globeletjs from "https://unpkg.com/globeletjs@latest/dist/globelet.js";
-const params = { container: 'globe',
-style: "./klokantech-basic-style-geojson.json",
-center: [-100, 38.5],
-altitude: 6280,
-};
-const globePromise = globeletjs.initGlobe(params);
